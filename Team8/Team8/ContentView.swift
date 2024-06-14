06310afb96f6930b30d4df7e9a539479edb07346
Junsung Kim <assd1108@naver.com>
2024-06-15 01:30:15 +0900

    [Config] Configure Project

---
+++ Team8/Team8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team8
+//
+//  Created by 김준성 on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
